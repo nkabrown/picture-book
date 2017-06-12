23ec00d1b190ae72260b3ea2559b3987874e7d18
Nathan Brown <nkabrown@gmail.com>
2017-06-11 21:34:57 -0400

    scrape iso 639-1 two letter language codes

---
+++ scrapers/scrape_language_codes.py
@@ -0,0 +1,25 @@
+import csv
+from lxml import html
+import requests
+
+class scrapeISOCodes:
+    def __init__(self):
+        self.url = 'https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes'
+
+    def scrape(self):
+        page = requests.get(self.url)
+        tree = html.fromstring(page.content)
+        table = tree.xpath('//table[contains(@class, "wikitable")]')
+        rows = tree.xpath('//tr')
+        language_names = tree.xpath('//tr/td[position()=3]/a[position()=1]/text()')
+        two_letter_codes = tree.xpath('//tr/td[position()=5]/text()')
+        self.write_to_csv(language_names, two_letter_codes)
+
+    def write_to_csv(self, names, codes):
+        outfile = open('iso_639_1_codes.csv', 'wb')
+        writer = csv.writer(outfile)
+        writer.writerow(['language', 'code'])
+        for i, name in enumerate(names):
+            writer.writerow([name.encode('utf-8'), codes[i]])
+
+scrapeISOCodes().scrape()
